fab5642251361f59b762a4a699a93612e17d65d8
dovahkiinemo <alexjmoulin@gmail.com>
2023-03-27 22:10:07 -0300

    adiciona funcao divisao

--- projetos/calculadora/calculadora/funcoes.py
+++ projetos/calculadora/calculadora/funcoes.py
@@ -15,3 +15,10 @@ def multiplicacao(x,y):
         return x * y
     except TypeError:
         print(f"O input 'a' e 'b' devem ser um int/float, recebido {a}, {type(a)}, e {b},  {type(b)}")
+
+def divisao(x,y):
+    if y == 0:
+        print("Divisão inválida, denominador igual a zero.")
+        return 0
+    else:
+        return x/y
