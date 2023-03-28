1ba68345011a017c9f649befba58fca40d8c9b38
dovahkiinemo <alexjmoulin@gmail.com>
2023-03-27 22:04:31 -0300

    adiciona funcao subtracao

--- projetos/calculadora/calculadora/funcoes.py
+++ projetos/calculadora/calculadora/funcoes.py
@@ -3,3 +3,9 @@ def soma(x,y):
         return x + y
     except TypeError:
         print(f"O input 'a' e 'b' devem ser um int/float, recebido {a}, {type(a)}, e {b},  {type(b)}")
+
+def subtracao(x,y):
+    try:
+        return x - y
+    except TypeError:
+        print(f"O input 'a' e 'b' devem ser um int/float, recebido {a}, {type(a)}, e {b},  {type(b)}")
